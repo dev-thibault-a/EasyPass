91941e78a0c8fa3092035e52ad08655b06468adb
Thibault A. <thibault_alexis@icloud.com>
2025-10-26 21:53:47 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('easypass').then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+      '/app.js',
+      '/manifest.json',
+      '/icon-192.png',
+      '/icon-512.png'
+    ]))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
